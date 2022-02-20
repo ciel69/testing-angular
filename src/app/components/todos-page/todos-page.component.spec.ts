81f9c18c6502bb21b419a5904473d092cff93d8d
Alex <bukhaltsov@lanit.ru>
2022-02-20 13:49:09 +0300

    fix test

--- src/app/components/todos-page/todos-page.component.spec.ts
+++ src/app/components/todos-page/todos-page.component.spec.ts
@@ -3,17 +3,17 @@ import {NO_ERRORS_SCHEMA} from '@angular/core';
 import {By} from '@angular/platform-browser';
 
 import {TodosPageComponent} from './todos-page.component';
-import {TodosService, TodosStore} from '../../store/todos';
+import {TodosQuery, TodosService} from '../../store/todos';
 
 describe('TodosPageComponent', () => {
   let component: TodosPageComponent;
-  let todosStore: TodosStore;
   let fixture: ComponentFixture<TodosPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       providers: [
         TodosService,
+        TodosQuery
       ],
       declarations: [TodosPageComponent],
       schemas: [NO_ERRORS_SCHEMA]
@@ -24,7 +24,6 @@ describe('TodosPageComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodosPageComponent);
     component = fixture.componentInstance;
-    todosStore = TestBed.inject(TodosStore);
     fixture.detectChanges();
   });
 
@@ -33,7 +32,6 @@ describe('TodosPageComponent', () => {
   });
 
   it('should display no todos message', () => {
-    fixture.detectChanges();
     const noMessageElement = fixture.debugElement.query(By.css('.no-todos'));
     expect(noMessageElement).not.toBeNull();
   });
